e9a8c8e43f2d109d5eecaa2d66303d0a7ef66ff9
Mateo Victorica <3951@holbertonschool.com>
2021-09-30 07:36:25 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
-#include <stdio.h>
+#include<stdio.h>
+/*main function displays text using puts*/
 
-/**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle);
-	    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+
