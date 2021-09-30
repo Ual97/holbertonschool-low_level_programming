fed3b696dd026aa1d378699cb95cfbcfcdfde62e
Mateo Victorica <3951@holbertonschool.com>
2021-09-30 10:15:21 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,9 +12,9 @@ int main(void)
 	long long int longlongintsize;
 
 	printf("Size of a char : %zu Byte(s)\n", sizeof(charsize));
-	printf("Size of a float : %zu Byte(s)\n", sizeof(floatsize));
-	printf("Size of a long int : %zu Byte(s)\n", sizeof(longintsize));
 	printf("Size of an int : %zu Byte(s)\n", sizeof(intsize));
+	printf("Size of a long int : %zu Byte(s)\n", sizeof(longintsize));
 	printf("Size of a long long int : %zu Byte(s)\n", sizeof(longlongintsize));
+	printf("Size of a float : %zu Byte(s)\n", sizeof(floatsize));
 	return (0);
 }
