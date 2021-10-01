3c2fd29014ba01ea8d02e8a346056e601849e5b9
Mateo Victorica <3951@holbertonschool.com>
2021-10-01 10:22:52 -0700

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main-function gives last digit form SRAND
+*Return: 0
+*/
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ++ch)
+	{
+		putchar (ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ++ch)
+	{
+		putchar (ch);
+	}
+	putchar ('\n');
+	return (0);
+}
