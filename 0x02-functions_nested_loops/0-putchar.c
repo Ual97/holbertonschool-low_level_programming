68903c8e2c691281f540f4a66202a98479500782
Mateo Victorica <3951@holbertonschool.com>
2021-10-05 07:04:42 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*main-function gives last digit form SRAND
+*Return: 0
+*/
+
+int main(void)
+{
+	char *ch = "_putchar";
+
+	while (*ch)
+	{
+		_putchar(*ch);
+		ch++;
+	}
+	_putchar ('\n');
+
+	return (0);
+}
