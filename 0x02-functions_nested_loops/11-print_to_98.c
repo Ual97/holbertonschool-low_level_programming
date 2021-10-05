3aa9ea3a56f2994441264bc8b476ddf874da0949
Mateo Victorica <3951@holbertonschool.com>
2021-10-05 15:24:22 -0700

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,21 +8,20 @@
 
 void print_to_98(int n)
 {
-	while (n > 98)
-	{
-		printf("%d, ", n);
-		n--;
-	}
-	printf("98");
-	if (n == 98)
-	{
-		printf("%d", n);
-	}
 	while (n < 98)
 	{
 		printf("%d, ", n);
 		n++;
 	}
-	printf("98");
+
+	while (n > 98)
+	{
+		printf("%d, ", n);
+		n--;
+	}
+
+	if (n == 98)
+		printf("%d", n);
+
 	printf("\n");
 }
