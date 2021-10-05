0acd89a1e8e0fbf11b6c81d552405671f75ae4ca
Mateo Victorica <3951@holbertonschool.com>
2021-10-05 14:39:00 -0700

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+*add - a function gives last digit form SRAND
+*@x: first additive
+*@y: second additive
+*Return: 1 if c is lowercase and 0 otherwise
+*/
+
+int add(int x, int y)
+{
+	return (x + y);
+}
