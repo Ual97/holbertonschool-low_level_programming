b61deb94e49053b6212704e155ec4b334b0437ec
Mateo Victorica <3951@holbertonschool.com>
2021-10-05 08:12:50 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,12 +8,13 @@
 void print_alphabet_x10(void)
 {
 
-	int i,n;
+	int i, n;
+
 	n = 0;
-	
-	while (n<10)
+
+	while (n < 10)
 	{
-		for(i = 'a'; i <= 'z'; i++)
+		for (i = 'a'; i <= 'z'; i++)
 		{
 			_putchar(i);
 		}
