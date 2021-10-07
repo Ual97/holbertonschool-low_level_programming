1c22a9944de815ef6b91998bc2a06147da7cca0f
Mateo Victorica <3951@holbertonschool.com>
2021-10-07 06:55:18 -0700

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+*more_numbers - a function gives last digit form SRAND
+*Return: x10 a-z
+*/
+
+void more_numbers(void)
+{
+
+	int i, n;
+
+	n = 0;
+
+	while (n < 10)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+			{
+				 _putchar((i / 10) + 48);
+			}
+			_putchar((i % 10) + 48);
+		}
+		n++;
+		_putchar('\n');
+	}
+}
