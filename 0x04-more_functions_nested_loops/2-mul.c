e8609f806569639db0a492beb5d47afb378ed0e7
Mateo Victorica <3951@holbertonschool.com>
2021-10-07 06:09:25 -0700

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+*mul - a function gives last digit form SRAND
+*@a: first additive
+*@b: second additive
+*Return: 1 if c is lowercase and 0 otherwise
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
