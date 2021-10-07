9f24b961b235890ddd364ab48a9bfcb3fd562612
Mateo Victorica <3951@holbertonschool.com>
2021-10-07 10:15:33 -0700

    7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+*print_diagonal - a function gives last digit form SRAND
+*@n: the int value
+*Return: 1 if c is lowercase and 0 otherwise
+*/
+
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
