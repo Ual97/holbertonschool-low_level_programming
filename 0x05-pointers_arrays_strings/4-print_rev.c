e249508a8dd4310e55c04c6e0be84e545efce69e
Mateo Victorica <3951@holbertonschool.com>
2021-10-12 11:55:58 -0700

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,11 +10,10 @@ void print_rev(char *s)
 {
 	int i;
 
-	for (i = 0; s[i] != 0;)
+	for (i = 0; s[i] != 0; i++)
 	{
-		i++;
 	}
-	for (i--; s[i] >= 0; i--)
+	for (i--; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
