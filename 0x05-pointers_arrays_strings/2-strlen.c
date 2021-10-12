a2db033cd40ff799eacf0f0957dbe968ccfce417
Mateo Victorica <3951@holbertonschool.com>
2021-10-12 08:57:08 -0700

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*_strlen - function gives last digit form SRAND
+*@s: input
+*Return: 0
+*/
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; i <= *s; i++)
+	{
+	}
+	return (i);
+}
