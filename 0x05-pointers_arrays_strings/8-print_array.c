a1f72a2a80abb8e0cf4f8a51d2c6bc493a437a74
Mateo Victorica <3951@holbertonschool.com>
2021-10-12 16:39:35 -0700

    8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  *print_array - a function that prints arrays in order
+  *@a: array input
+  *@n: n input
+  */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i <= (n - 1); i++)
+	{
+		if (i == (n - 1))
+		{
+			printf("%d", a[n - 1]);
+		}
+		else
+		{
+			printf("%d, ", a[i]);
+		}
+	}
+	printf("\n");
+}
