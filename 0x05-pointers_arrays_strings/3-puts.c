2918ad7e52f0fd0c62c1d42e80c58232b07e57a2
Mateo Victorica <3951@holbertonschool.com>
2021-10-12 09:18:21 -0700

    3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*_puts - function gives last digit form SRAND
+*@str: input
+*Return: 0
+*/
+
+void _puts(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str);
+		*str++;
+	}
+	_putchar('\n');
+}
