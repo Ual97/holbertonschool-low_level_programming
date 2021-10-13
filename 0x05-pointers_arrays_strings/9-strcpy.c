789092f7778d1819bd96f993f71737c587d6fcfb
Mateo Victorica <3951@holbertonschool.com>
2021-10-12 17:07:22 -0700

    9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  *_strcpy - a function that copies another string into a buufer
+  *@dest: destination buffer
+  *@src: source string
+  */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	return (dest);
+}
