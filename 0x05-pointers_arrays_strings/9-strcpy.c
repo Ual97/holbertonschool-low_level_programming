8eba540079fc3fa847693098f46b3a3f9b9554d8
Mateo Victorica <3951@holbertonschool.com>
2021-10-12 17:11:32 -0700

    9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,7 +11,7 @@ char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	for (i = 0; src[i] != '\0'; i++)
-		dest[i] = src[i + 1];
+	for (i = 0; src[i] == '\0'; i++)
+		dest[i] = src[i];
 	return (dest);
 }
