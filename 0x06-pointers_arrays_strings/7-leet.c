275a028d9dafa633d7b2289a8b214ea3d47fad9a
Mateo Victorica <3951@holbertonschool.com>
2021-10-15 13:26:41 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *leet - a function that replaces certain letters for numbers
+ *@n: string input
+ *Return: tranlated string
+ */
+
+char *leet(char *n)
+{
+	int i, j;
+	int ltr[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int nmr[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+
+	for (i = 0; n[i] != 0; i++)
+	{
+		for (j = 0; ltr[j] != 0; j++)
+		{
+			if (n[i] == leet[j])
+			{
+				n[i] = nmr[j];
+			}
+		}
+	}
+	return (n);
+}
