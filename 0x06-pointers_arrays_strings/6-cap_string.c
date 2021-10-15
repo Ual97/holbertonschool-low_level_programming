8cca4837da16696e57703a8bc834c93907defa6a
Mateo Victorica <3951@holbertonschool.com>
2021-10-15 10:49:23 -0700

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,6 +12,10 @@ char *cap_string(char *n)
 
 	for (i = 0; n[i] != 0; i++)
 	{
+		if (n[i] >= 97 && n[i] <= 122)
+		{
+			n[i] = n[i] - 32;
+		}
 		for (j = 0; separators[j] != 0; j++)
 		{
 			if (n[i] == separators[j])
