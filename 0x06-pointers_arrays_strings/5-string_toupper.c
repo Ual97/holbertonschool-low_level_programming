c75ecd3115a3043342afd179af831d791f537d75
Mateo Victorica <3951@holbertonschool.com>
2021-10-15 05:51:50 -0700

    string to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ **string_toupper - function that changes lowercase letters to uppercase
+ *@n: input string
+ *Return: Uppercase string
+ */
+
+char *string_toupper(char *n)
+{
+	int i;
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		if (n[i] >= 97 && n[i] <= 122)
+		{
+			n[i] = n[i] - 32;
+		}
+	}
+	return(n);
+}
