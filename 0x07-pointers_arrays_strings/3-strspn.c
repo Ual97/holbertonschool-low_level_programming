7392641c45ecd88aa1a30ea3313fcb188df1c328
Mateo Victorica <3951@holbertonschool.com>
2021-10-18 15:43:23 -0700

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,17 +10,24 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j;
+	int i, j, marca;
 	int count = 0;
 
 	for (i = 0; s[i] != 0; i++)
 	{
+		marca = 0;
+
 		for (j = 0; accept[j] != 0; j++)
 		{
 			if (s[i] == accept[j])
 			{
 				count++;
+				marca = 1;
 			}
+		}
+		if (marca == 0)
+		{
+			return (count);
 		}
 	}
 	return (count);
