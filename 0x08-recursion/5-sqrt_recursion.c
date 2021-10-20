58b834fb79cab4c5dac07fa36f1f0b532ed18765
Mateo Victorica <3951@holbertonschool.com>
2021-10-20 16:52:37 -0700

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -25,6 +25,9 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	else
-		return (square_root(n, (n + 1) / 2));
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (square_root(n, (n + 1) / 2));
 }
