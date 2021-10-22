e04242027d3457bd4d094d3331a4c764ce8655f8
Mateo Victorica <3951@holbertonschool.com>
2021-10-22 09:04:40 -0700

    1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,15 +10,6 @@
 
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-	int i = 0;
-
-	if (argc > 1)
-	{
-		for (i = 0; i < argc; i++)
-		{
-		}
-	}
-	i = i - 1;
-	printf("%d\n", i);
+	printf("%d\n", argc - 1);
 	return (0);
 }
