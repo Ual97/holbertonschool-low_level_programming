92d74295382354f4a1500c2791f5f6eb1f7e7374
Mateo Victorica <3951@holbertonschool.com>
2021-10-26 06:58:04 -0700

    create_array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	array = malloc(sizeof(char) * size);
-	if (size == 0)
+	if (size == 0 || array == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
 		array[i] = c;
