3dd3f02c120fe4420d88a796da97d99b617863e1
Mateo Victorica <3951@holbertonschool.com>
2021-10-26 11:39:33 -0700

    array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,9 +24,9 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		ar[i] = malloc(sizeof(int) * width);
-		if (ar[i] == NULL)
+		if (ar == NULL)
 		{
-			free(ar[i]);
+			free(ar);
 			return (NULL);
 		}
 		for (j = 0; j < width; j++)
