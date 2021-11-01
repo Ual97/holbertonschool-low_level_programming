559960aaed8325592c5831312b21e402810b9859
Mateo Victorica <3951@holbertonschool.com>
2021-11-01 11:55:39 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a struct that takes name, owner and age of a dog
+ * @name: 1st member
+ * @age: 2nd member
+ * @owner: 3d member
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
