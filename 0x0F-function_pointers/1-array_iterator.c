8a577b84cd9a67276a6784ce3255f313958980e9
Mateo Victorica <3951@holbertonschool.com>
2021-11-03 12:02:14 -0700

    1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - a function that given parameters iterates an array
+ * @size: size of array
+ * @action: pointer to the "to do" action for the array
+ * @array: input array
+ * Retrun: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+	if (array && action)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
+}
