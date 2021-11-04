842620e4397083e23f28a7731871a6149da289d2
Mateo Victorica <3951@holbertonschool.com>
2021-11-04 12:51:48 -0700

    1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,15 +14,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list ptr;
-	int temp;
 
 	if (separator == NULL)
 		return;
 	va_start(ptr, n);
 	for (i = 0; i < n; i++)
 	{
-		temp = va_arg(ptr, int);
-		printf("%d", temp);
+		printf("%d", va_arg(ptr, const unsigned int));
 		if (i != (n - 1))
 			printf("%s", separator);
 	}
