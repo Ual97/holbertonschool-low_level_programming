651b6fcf4fdada438a453497d392b58bc13ba97e
Mateo Victorica <3951@holbertonschool.com>
2021-11-11 13:44:12 -0800

    first 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * print_list - function that prints the elemnts of a linked list
+ * @h: struct input
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	unsigned int length = 0;
+
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d]%s\n", 0, "(nil)");
+		else
+			printf("[%d]%s\n", h->len, h->str);
+		h = h->next;
+		length++;
+	}
+	return (length);
+}
