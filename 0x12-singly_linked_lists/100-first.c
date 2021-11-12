85f76bcc2f7c9cac28e76e5369b6cae8dd28463c
Mateo Victorica <3951@holbertonschool.com>
2021-11-11 16:15:59 -0800

    100 v.2

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,13 @@
 #include "lists.h"
 
-/* myStartupFan - a function that executes before main
+/**
+ * myStartupFun - a function that executes before main
+ *
  * Return: nothing void
  */
 
-void myStartupFun (void)
+void myStartupFun(void)
 {
-    printf ("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
