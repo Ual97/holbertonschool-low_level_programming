a4451f423afe2f5ed3ff24006d8c130c9b8076cf
Mateo Victorica <3951@holbertonschool.com>
2021-11-11 16:09:27 -0800

    100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/* myStartupFan - a function that executes before main
+ * Return: nothing void
+ */
+
+void myStartupFun (void)
+{
+    printf ("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
