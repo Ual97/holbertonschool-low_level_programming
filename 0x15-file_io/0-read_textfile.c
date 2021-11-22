1fc75168a0e426312769768870d974c0ee44f597
Mateo Victorica <3951@holbertonschool.com>
2021-11-22 09:21:42 -0800

    0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,7 +19,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	buff = malloc(sizeof(char) * letters);
+	buff = malloc(sizeof(char) * letters + 1);
 	if (buff == NULL)
 	{
 		free(buff);
