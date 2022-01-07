6aac690fc13d6d2c3a430a4985155a15d2af1413
Mateo <3951@holbertonschool.com>
2022-01-07 17:14:05 -0300

    first push

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+int main(void)
+{
+	int intsize;
+	float floatsize;
+	char charsize;
+	long int longintsize;
+	long long int longlongintsize;
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(charsize));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intsize));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longintsize));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongintsize));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatsize));
+	return (0);
+}
